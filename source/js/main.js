90a6b5f46d74e56304881e9e78f2bf71d0996182
Anastasiia <nastyara13@yandex.ru>
2023-09-19 15:52:05 +0300

    add

--- source/js/main.js
+++ source/js/main.js
@@ -2,22 +2,95 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 
+import {
+  getSwiperHero,
+  getSwiperProgramms,
+  getSwiperNews,
+  getSwiperReviews
+} from './modules/slider';
+import {
+  openMenu,
+  openDropMenuProgramm,
+  openDropMenuNews
+} from './modules/open-menu';
+import {initAccordions} from './modules/accordion/init-accordion';
+import {CustomSelect} from './modules/select/custom-select';
+
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-
   // Utils
   // ---------------------------------
-
   iosVhFix();
+  getSwiperHero();
+  getSwiperProgramms();
+  getSwiperNews();
+  getSwiperReviews();
+  openMenu();
+  openDropMenuProgramm();
+  openDropMenuNews();
+  initAccordions();
 
   // Modules
   // ---------------------------------
+
+  /* eslint-disable no-undef */
+  const map = L.map('map')
+      .on('load', () => {})
+      .setView(
+          {
+            lat: 55.028522,
+            lng: 82.928281,
+          },
+          17
+      );
+  map.on('mouseout', function () {
+    map.scrollWheelZoom.disable();
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
+  }).addTo(map);
+
+  const mainPinIcon = L.icon({
+    iconUrl: './img/svg/pin.svg',
+    iconSize: [38, 50],
+    iconAnchor: [30, 55],
+  });
+
+  const mainPinMarker = L.marker(
+      {
+        lat: 55.028522,
+        lng: 82.928281,
+      },
+      {
+        icon: mainPinIcon,
+      }
+  );
+
+  mainPinMarker.addTo(map);
+
+  mainPinMarker.on('moveend', (evt) => {
+    evt.target.getLatLng();
+  });
+
+  map.setView(
+      {
+        lat: 55.028522,
+        lng: 82.928281,
+      },
+      17
+  );
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
+
+    const select = new CustomSelect();
+    select.init();
+
     const form = new Form();
     window.form = form;
     form.init();
